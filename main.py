ac68a830f460efaf1e2c938b9fd13dd1010b4c2b
cuongdev <cuongdev@nexondv.com>
2024-07-10 17:57:36 +0700

    second commit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse
 import os
 import whisper
 import datetime
+import requests
+import uuid
 
 app = FastAPI()
 
@@ -18,23 +20,33 @@ UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 # Load the Whisper model (you can choose a specific model size like 'base', 'small', 'medium', 'large')
-model = whisper.load_model("base")
+model = whisper.load_model("medium")
 
-@app.post("/upload-video/")
+@app.post("/transcribe/")
 async def upload_video(
-    file: UploadFile = File(...), 
+    # file: UploadFile = File(...), 
+    video_url:str,
     language: str = Query(None, description="Language of the video (optional)")
 ):
-    # Check file type
-    if not file.filename.endswith(('.mp4', '.mov', '.avi', '.mkv')):
-        raise HTTPException(status_code=400, detail="Invalid file format")
+    try:
+        response = requests.get(video_url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=400, detail=f"Error downloading video: {e}")
 
-    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    # # Check file type
+    # if not file.filename.endswith(('.mp4', '.mov', '.avi', '.mkv')):
+    #     raise HTTPException(status_code=400, detail="Invalid file format")
+
+    # file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    # Save the video to a temporary file
+    file_path = os.path.join(UPLOAD_DIR, uuid.uuid4())
 
     # Save the uploaded file
     with open(file_path, "wb") as f:
-        content = await file.read()
-        f.write(content)
+        # content = await file.read()
+        f.write(response.content)
 
     # Transcribe the video using Whisper
     transcription = transcribe_video(file_path, language)
@@ -45,7 +57,7 @@ async def upload_video(
     except Exception as e:
         return JSONResponse(content={"error": f"Failed to delete file: {str(e)}"}, status_code=500)
 
-    return JSONResponse(content={"filename": file.filename, "transcription": transcription}, status_code=201)
+    return JSONResponse(content={"transcription": transcription}, status_code=201)
 
 def transcribe_video(file_path: str, language: str = 'vi') -> str:
     # Use Whisper to transcribe the video        
